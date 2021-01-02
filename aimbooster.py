01e399d724af472b4cd09130f78ade192df5f5e3
Sayonara0 <75174547+Sayonara0@users.noreply.github.com>
2021-01-03 01:24:18 +0300

    Update aimbooster.py

--- aimbooster.py
+++ aimbooster.py
@@ -12,9 +12,9 @@ def click(x,y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
-#Color of center: r,g,b(255, 219, 195)
 
-while keyboard.is_pressed('q') == False:
+
+while keyboard.is_pressed('q') == False: #for stop bot press 'q'
 
     pic = pyautogui.screenshot(region=(660,350,600,400))
 
@@ -22,8 +22,8 @@ while keyboard.is_pressed('q') == False:
 
     for x in range(0,width,5):
         for y in range(0,height,5):
-
-            r,g,b = pic.getpixel((x,y))
+            #Color of center: r,g,b(255, 219, 195)
+            r,g,b = pic.getpixel((x,y)) # r=red, g=green, b=blue
 
             if b == 195:
                 click(x+660,y+350)
